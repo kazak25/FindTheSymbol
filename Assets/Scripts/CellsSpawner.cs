131b377d49fdf795b72a9d2acfd4cb53d9e039a0
MakeZzzzz <changeyourselfyesterday@gmail.com>
2023-02-13 23:23:15 +0300

    first commit

---
+++ Assets/Scripts/CellsSpawner.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellsSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _cell;
+    private float _x = -3;
+    private float _y = 3;
+    private void Start()
+    {
+        HardLevel();
+
+    }
+
+    private void Easylevel()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            var cell = Instantiate(_cell);
+            _cell.transform.position = new Vector3(_x, 0, 0);
+            _x += 5;
+        }
+    }
+    
+    private void MediumLevel()
+    {
+        for (int k = 0; k < 2; k++)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                var cell = Instantiate(_cell);
+                _cell.transform.position = new Vector3(_x, _y, 0);
+                _x += 3;
+            }
+            _x = -3;
+            _y -= 3;
+        }
+       
+    }
+    private void HardLevel()
+    {
+        for (int k = 0; k < 3; k++)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                var cell = Instantiate(_cell);
+                _cell.transform.position = new Vector3(_x, _y, 0);
+                _x += 3;
+            }
+            _x = -3;
+            _y -= 3;
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScriptableObject : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> _cars;
+    [SerializeField] private List<Sprite> _letters;
+    [SerializeField] private List<Sprite> _numbers;
+}
