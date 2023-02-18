c85e196e9a72b0f67bad937ab8774af25e9fd43a
kazak25 <kazak77709@gmail.com>
2023-02-18 12:53:13 +0300

    preFinal update

--- Assets/Scripts/RayCast.cs
+++ Assets/Scripts/RayCast.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using DG.Tweening;
 using JetBrains.Annotations;
 using TMPro;
 using Unity.VisualScripting;
@@ -9,7 +10,9 @@ using UnityEditor.UIElements;
 using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 public class RayCast : MonoBehaviour
 {
@@ -21,9 +24,19 @@ public class RayCast : MonoBehaviour
     [SerializeField] private Easy _easyLevelState;
     [SerializeField] private Medium _mediumLevelState;
     [SerializeField] private Hard _hardLevelState;
+    [SerializeField] private GameObject _nextLevelbutton;
+    [SerializeField] private GameObject _restartButton;
     public UnityEvent _PlayMode;
     private bool _isGameActive = false;
     private int _levelNumber;
+    private bool isLastLevel = false;
+    private bool _isWinCondition = false;
+    [SerializeField] private AudioSource _win;
+    [SerializeField] private AudioSource _fail;
+    [SerializeField] private CanvasGroup _canvasGroup;
+    
+
+    
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -34,43 +47,101 @@ public class RayCast : MonoBehaviour
 
     private void Start()
     {
-        _stateMachine = new StateMachine(_easyLevelState,_mediumLevelState,_hardLevelState);
+        _stateMachine = new StateMachine(_easyLevelState, _mediumLevelState, _hardLevelState);
+        BlackOut();
     }
 
     private void Raycast()
     {
+        if (_isWinCondition)
+        {
+            return;
+        }
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, 10000))
         {
+            var startPosition = hit.collider.gameObject.transform.position;
             var name = hit.collider.gameObject.name;
             ObjectsSelection(name);
-            if (_isGameActive == true && hit.collider.gameObject.name == _gameMode.tempName )
+            if (_isGameActive && hit.collider.gameObject.name == _gameMode.tempName)
             {
-                Debug.Log("Мдээээ");
+                _win.Play();
+                _isWinCondition = true;
+                var scale = hit.collider.gameObject.transform.localScale;
+                hit.collider.gameObject.transform.DOScale(new Vector3(1.5f, 1.5f, 0), 1f).OnComplete(() =>
+                    hit.collider.gameObject.transform.DOScale(scale, 1f));
+
                 _levelNumber++;
-                ChangeLevel(_levelNumber);
+                if (isLastLevel)
+                {
+                    StartCoroutine(ShowButtonRestart());
+
+                    return;
+                }
+
+                StartCoroutine(ShowButtonNextLevel());
+
+            }
+
+            else
+            {
+                _fail.Play();
+                hit.collider.gameObject.transform.DOShakePosition(1f, 0.5f)
+                    .OnComplete(() => hit.collider.gameObject.transform.position = startPosition);
             }
         }
     }
 
-    private void ChangeLevel(int n)
+    public void BlackOut()
     {
-        switch (n)
+        _canvasGroup.DOFade(1, 5f);
+    }
+
+    public void WinCondition()
+    {
+        _isWinCondition = false;
+    }
+
+    IEnumerator ShowButtonNextLevel()
+    {
+        yield return new WaitForSeconds(2);
+        _nextLevelbutton.SetActive(true);
+    }
+    IEnumerator ShowButtonRestart()
+    {
+        yield return new WaitForSeconds(2);
+        _restartButton.SetActive(true);
+    }
+    
+    public void ChangeLevel()
+    {
+        switch (_levelNumber)
         {
             case 1:
             {
                 _stateMachine.Enter<Medium>();
+                _nextLevelbutton.SetActive(false);
+                _PlayMode.Invoke();
                 break;
             }
             case 2:
             {
                 _stateMachine.Enter<Hard>();
+                isLastLevel = true;
+                _nextLevelbutton.SetActive(false);
+                _PlayMode.Invoke();
                 break;
             }
            default: return;
                
         }
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadSceneAsync(GlobalConstants.SceneGame);
     }
 
 
