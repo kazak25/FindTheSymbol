0ed0c1b952a3e8ab6906352b582a5bed5005e449
kazak25 <kazak77709@gmail.com>
2023-02-23 10:47:01 +0300

    TEST

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour, IStateWithoutContext
+{
+    // Start is called before the first frame update
+    [SerializeField] private CellsSpawner _levelDifficult;
+    [SerializeField] private GameObject _currentLevel;
+    
+   // private GameController _gameController;
+    private List<Sprite> _sprites = new List<Sprite>();
+    private StateMachine _stateMachine;
+   
+    
+    public void Array(List<Sprite> sprites)
+    {
+        _sprites = sprites;
+    }
+
+    public void Initialize(StateMachine stateMachine)
+    {
+        _stateMachine = stateMachine;
+    }
+
+    public void Enter()
+    {
+       
+    }
+
+    public void Exit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
